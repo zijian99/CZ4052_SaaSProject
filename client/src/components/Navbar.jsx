e1ba6fe949644b90ea4ed8a1d30cd49c4f1592ed
ZJ <zijianchua0@gmail.com>
2024-04-09 00:18:05 +0800

    implemented sidebar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -14,7 +14,11 @@ import { setMode } from 'state';
 import profileImage from "assets/profile.jpeg";
 import { AppBar, Icon, IconButton, InputBase, Toolbar, useTheme } from '@mui/material';
 
-const Navbar = () => {
+
+const Navbar = ({
+    isSidebarOpen,
+    setIsSidebarOpen,
+}) => {
     const dispatch = useDispatch();
     const theme = useTheme()    
     return (
@@ -26,36 +30,36 @@ const Navbar = () => {
         }}
     >
         <Toolbar sx={{justifyContent: "space-between"}}>
-            {/* LEFT SIDE OF NAVBAR*/}
-            <FlexBetween>
-                <IconButton onClick={() => console.log('open/close sidebar')}>
-                    <MenuIcon/>
-                </IconButton>
-                <FlexBetween
-                    backgroundColor={theme.palette.background.alt}
-                    borderRadius="9px"
-                    gap="3rem"
-                    p="0.1rem 1.5rem"
-                >
-                    <InputBase placeholder='Search...'/>
-                    <IconButton>
-                        <Search/>
-                    </IconButton>
-                </FlexBetween>
-            </FlexBetween>
+            {/* LEFT SIDE */}
+        <FlexBetween>
+          <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+            <MenuIcon />
+          </IconButton>
+          <FlexBetween
+            backgroundColor={theme.palette.background.alt}
+            borderRadius="9px"
+            gap="3rem"
+            p="0.1rem 1.5rem"
+          >
+            <InputBase placeholder="Search..." />
+            <IconButton>
+              <Search />
+            </IconButton>
+          </FlexBetween>
+        </FlexBetween>
 
-            {/* RIGHT SIDE OF NAVBAR*/}
-            <FlexBetween gap="1.5rem">
-                <IconButton onClick={() => dispatch(setMode())}>
-                    {theme.palette.mode === 'dark' ? (
-                        <DarkModeOutlined sx={{ fontSize: "25px" }}/>
-                    ) : (
-                        <LightModeOutlined sx={{ fontSize: "25px" }}/>
-                    )}
-                </IconButton>
-                <IconButton>
-                    <SettingsOutlined/>
-                </IconButton>
+        {/* RIGHT SIDE */}
+        <FlexBetween gap="1.5rem">
+          <IconButton onClick={() => dispatch(setMode())}>
+            {theme.palette.mode === "dark" ? (
+              <DarkModeOutlined sx={{ fontSize: "25px" }} />
+            ) : (
+              <LightModeOutlined sx={{ fontSize: "25px" }} />
+            )}
+          </IconButton>
+          <IconButton>
+            <SettingsOutlined sx={{ fontSize: "25px" }} />
+          </IconButton>
             </FlexBetween>
 
         </Toolbar>
--- client/src/scenes/layout/index.js
+++ client/src/scenes/layout/index.js
@@ -4,16 +4,30 @@ import { Box, useMediaQuery } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Navbar from "components/Navbar";
+import Sidebar from "components/Sidebar";
 
 
 const Layout = () => {
-  return <Box width="100%" height="100%">
-    <Box>
-      <Navbar/>
-      {/* Outlet is used for rendering the page contents */}
-      <Outlet/>
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  return (
+    <Box display={isNonMobile? "flex" : "block"} width="100%" height="100%">
+      <Sidebar
+        isNonMobile={isNonMobile}
+        drawerWidth="250px"
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+      <Box flexGrow={1}>
+        <Navbar
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
+        {/* Outlet is used for rendering the page contents */}
+        <Outlet/>
+      </Box>
     </Box>
-  </Box>
-};
+)};
 
 export default Layout;
